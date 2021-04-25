7ab29756b18b4596985bea23db55a260b3639c06
QianYiYi <2441168440@qq.com>
2021-04-25 13:34:05 +0800

    modify router

--- src/router/index.js
+++ src/router/index.js
@@ -45,30 +45,4 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
-// router.beforeEach((to, from, next) => {
-//   // window.document.title = to.meta.title;
-//   console.log(to)
-//   // router.beforeEach((to, from, next) => {
-//   //   if (to.matched.some(record => record.meta.requiresAuth)) {
-//   //     // this route requires auth, check if logged in
-//   //     // if not, redirect to login page.
-//   //     if (!auth.loggedIn()) {
-//   //       next({
-//   //         path: '/login',
-//   //         query: { redirect: to.fullPath }
-//   //       })
-//   //     } else {
-//   //       next()
-//   //     }
-//   //   } else {
-//   //     next() // 确保一定要调用 next()
-//   //   }
-//   // })
-//   next()
-// })
-  // router.beforeEach((to, from, next) => {
-  //   console.log(to.matched)
-  //   console.log(from)
-  //   next()
-  // })
 export default router
